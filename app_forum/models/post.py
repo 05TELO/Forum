7914bc030c1aaba9e985daae8d1b811124f89d1a
Yauheni_Liubimov <0.5TELOO@gmail.com>
2023-02-17 13:40:31 +0300

    dd

--- app_forum/models/post.py
+++ app_forum/models/post.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True, null=True)
     text = models.TextField(max_length=1000)
     topic = models.ForeignKey(
-        Topic, on_delete=models.CASCADE, related_name="topic"
+        Topic, on_delete=models.CASCADE, related_name="posts"
     )
 
     def __str__(self):
